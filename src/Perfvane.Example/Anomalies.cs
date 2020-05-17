55801030d11c738349b560031ffe0cf3005a1c84
Andrey Dorokhov <aidmsu@gmail.com>
2020-05-18 01:10:48 +0300

    Minor changes 21

--- src/Perfvane.Example/Anomalies.cs
+++ src/Perfvane.Example/Anomalies.cs
@@ -12,7 +12,7 @@ namespace Perfvane.Example
 
         public static void Acceleration()
         {
-            Thread.Sleep(1004);
+            Thread.Sleep(1003);
         }
 
         public static void Bimodal()
