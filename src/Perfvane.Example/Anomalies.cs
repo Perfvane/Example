bbdc6a066c5b906c8daaff8362a7b0e774df87c7
Andrey Dorokhov <aidmsu@gmail.com>
2020-05-18 01:03:44 +0300

    Minor changes 13

--- src/Perfvane.Example/Anomalies.cs
+++ src/Perfvane.Example/Anomalies.cs
@@ -23,7 +23,7 @@ namespace Perfvane.Example
                     Thread.Sleep(508);
                     return;
                 case 1:
-                    Thread.Sleep(1009);
+                    Thread.Sleep(1007);
                     return;
                 default:
                     throw new NotImplementedException();
