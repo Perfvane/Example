e400b32bd8568381149bf8810c48839193bb1174
Andrey Dorokhov <aidmsu@gmail.com>
2020-05-18 01:09:30 +0300

    Minor changes 20

--- src/Perfvane.Example/Anomalies.cs
+++ src/Perfvane.Example/Anomalies.cs
@@ -20,7 +20,7 @@ namespace Perfvane.Example
             switch (new Random().Next(0, 2))
             {
                 case 0:
-                    Thread.Sleep(507);
+                    Thread.Sleep(506);
                     return;
                 case 1:
                     Thread.Sleep(1000);
